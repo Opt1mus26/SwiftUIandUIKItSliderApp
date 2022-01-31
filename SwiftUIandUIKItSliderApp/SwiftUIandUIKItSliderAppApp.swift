fe979d2a779a98721ec399d5599f88035a66b03a
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2022-01-31 18:35:23 +0300

    Initial Commit

---
+++ SwiftUIandUIKItSliderApp/SwiftUIandUIKItSliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIandUIKItSliderAppApp.swift
+//  SwiftUIandUIKItSliderApp
+//
+//  Created by Pavel Tsyganov on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIandUIKItSliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
